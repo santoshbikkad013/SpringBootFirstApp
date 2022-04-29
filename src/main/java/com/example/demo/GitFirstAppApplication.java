21b9c527811eb1ea77af4a3c427889c451c90440
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-04-29 11:17:30 -0800

    Git first project

---
+++ src/main/java/com/example/demo/GitFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFirstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/GitFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
