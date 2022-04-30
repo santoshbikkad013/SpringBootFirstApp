c38725d54db7b5469b50b8a95417cf2bd07bbd9c
Sagar4971 <sagarnerkar2912@gmail.com>
2022-04-30 12:26:19 +0530

    stashFile

--- src/main/java/com/example/model/Sagar.java
+++ src/main/java/com/example/model/Sagar.java
@@ -1,5 +1,54 @@
 package com.example.model;
 
 public class Sagar {
+	
+	private int id;
+	
+	private String name;
+	
+	private String Address;
+	
+	private String phoneNo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	@Override
+	public String toString() {
+		return "Sagar [id=" + id + ", name=" + name + ", Address=" + Address + ", phoneNo=" + phoneNo + "]";
+	}
+	
+	
+	
+	
 
 }
---
+++ src/main/java/com/sagar/OldTask.java
@@ -0,0 +1,13 @@
+package com.sagar;
+
+public class OldTask {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello...Good Morning");
+		
+		System.out.println("Welcome To Bikkad IT");
+		
+	}
+}
