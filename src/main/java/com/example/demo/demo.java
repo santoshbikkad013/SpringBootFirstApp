325c38f4c5b7a55b30efdc769bdf36dfddacee5f
JayPardeshi13 <jaypardeshi8855@gmail.com>
2022-04-30 12:02:55 +0530

    lecture finished

---
+++ src/main/java/com/example/demo/demo.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("lec at 2 pm");
+		System.out.println("lecture finished");
+	}
+	
+	
+}
